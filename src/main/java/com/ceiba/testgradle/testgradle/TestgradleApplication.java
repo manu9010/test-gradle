9472a9137a6b8ba02b4c2664be96d2ffd3dc4a6d
Manuel Enrique Cantillo Pareja - Ceiba Software <manuel.cantillo@ceiba.com.co>
2018-06-22 11:35:31 -0500

    Subiendo fuente base

---
+++ src/main/java/com/ceiba/testgradle/testgradle/TestgradleApplication.java
@@ -0,0 +1,12 @@
+package com.ceiba.testgradle.testgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestgradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestgradleApplication.class, args);
+	}
+}
